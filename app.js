86c5365d48d87abca10db3412b698bb10931be02
maheen <maheenzuhra@gmail.com>
2026-01-04 16:07:57 +0500

    solve 7q

---
+++ app.js
@@ -0,0 +1,24 @@
+// 3. Declare and initialize a strings array.
+
+var myStr = ["maheen", "hareem", "hiba", "hina"];
+
+// 4. Declare and initialize a numbers array.
+
+var myNum = [4, 5,6,7,8,9];
+
+// 5. Declare and initialize a boolean array.
+
+var myBoolean = [true , false];
+
+// 6. Declare and initialize a mixed array.
+
+var myArr = ["maheen", "hareem",  false , 5,6,7];
+
+// 7. Declare and Initialize an array and store available education qualifications in Pakistan 
+// (e.g. SSC, HSC, BCS, BS, BCOM, MS, M. Phil., PhD). 
+// Show the listed qualifications in your browser like:
+
+var edu = ["SSC", "HSC","BSC","BS","BCOM","MS","M.Phil","Phd"];
+
+document.write("<h1>Educations:</h1><ol><li>" + edu[0] + "</li><li>" + edu[1] + "</li><li>" + edu[2] + "</li><li>" + edu[3] +
+     "</li><li>" + edu[4] + "</li><li>" + edu[5] + "</li><li>" + edu[6] + "</li><li>" + edu[7] + "</li></ol>")
